50262903067800de5bf78ae0e745886f10bdb310
Hero's Dad <cody.monta@gmail.com>
2018-11-11 22:21:13 -0600

    first phase design complete

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBqXLjTvAXHCDVfAehYtcIPZThpJbNwCmI",
+    authDomain: "train-scheduler-7cdca.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7cdca.firebaseio.com",
+    projectId: "train-scheduler-7cdca",
+    storageBucket: "train-scheduler-7cdca.appspot.com",
+    messagingSenderId: "463759758424"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
