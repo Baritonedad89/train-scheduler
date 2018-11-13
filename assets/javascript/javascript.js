490e127e8ff7eb4ca198377093a0985c918fe4a4
Hero's Dad <cody.monta@gmail.com>
2018-11-12 21:37:12 -0600

    working on moment

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,16 +15,50 @@ var destination = "";
 var firstTtime = "";
 var frequency = "";
 
+
+
 $("#submit-btn").click(function(){
+
   var trainName = $("#trainName").val().trim()
   var destination = $("#destination").val().trim()
+  var frequency = $("#frequency").val().trim()
+  var firstTtime = $("#firstTtime").val().trim()
+
+
+  var firstTimeConverted = moment(firstTtime, "HH:mm");
+  console.log(`first train time last year: ${firstTimeConverted}`);
+
+  var convertTimeIntoUnix = moment(firstTimeConverted).format("X")
+  console.log(`first train time converted into unix time: ${convertTimeIntoUnix}`)
+
+  var thisMomentInUnix = moment().format("X");
+  console.log(`This moment in unix time ${thisMomentInUnix}`)
+
+  var diffTime = moment().diff(moment(firstTimeConverted, "X"), "minutes");
+  console.log(`difference in time: ${diffTime}`)
+
+  var tRemainder = diffTime % frequency;
+    console.log(`remaining time = ${tRemainder}`);
+
+    var tMinutesTillTrain = frequency - tRemainder;
+        console.log(`MINUTES TILL TRAIN: ${tMinutesTillTrain}`);
+
+
+
+
+
+
 
   $("#trainName").val("")
   $("#destination").val("")
+  $("#frequency").val("")
+  $("#firstTtime").val("")
+
 
   database.ref().push({
   trainName,
   destination,
+  frequency,
   dateAdded: firebase.database.ServerValue.TIMESTAMP
 
   });
@@ -35,6 +69,8 @@ database.ref().on("child_added", function (snapshot){
 $('tbody').append(`<tr>
                   <td>${sv.trainName}</td>
                   <td>${sv.destination}</td>
+                  <td>${sv.frequency}</td>
+
                   </tr>`
 
        );
