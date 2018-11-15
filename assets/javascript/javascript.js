6770fe555480545d6ea8665a8889631dbaf30817
Hero's Dad <cody.monta@gmail.com>
2018-11-15 12:10:08 -0600

    added github authorization- i think

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,6 +8,7 @@ $('document').ready(function() {
     projectId: "train-scheduler-7cdca",
     storageBucket: "train-scheduler-7cdca.appspot.com",
     messagingSenderId: "463759758424"
+    https://train-scheduler-7cdca.firebaseapp.com/__/auth/handler
   };
   firebase.initializeApp(config);
 
@@ -16,17 +17,12 @@ $('document').ready(function() {
 
   $("#submit-btn").click(function() {
     event.preventDefault();
-
     // grab values from the inputs
     var trainName = $("#trainName").val().trim()
     var destination = $("#destination").val().trim()
     var frequency = $("#frequency").val().trim()
     var firstTtime = $("#firstTtime").val().trim();
     console.log(firstTtime)
-
-
-
-
 
     // pushes the captured input on the firebase database
     database.ref().push({
@@ -35,41 +31,20 @@ $('document').ready(function() {
       frequency,
       firstTtime,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-
     });
+
     // clears out the input
     $("#trainName").val("")
     $("#destination").val("")
     $("#frequency").val("")
     $("#firstTtime").val("")
-
   })
-
 
   database.ref().on("child_added", function(snapshot) {
     var trainNameDB = snapshot.val().trainName
     var destinationDB = snapshot.val().destination
     var frequencyDB = snapshot.val().frequency
     var firstTtimeDB = snapshot.val().firstTtime
-
-    function getDataUpdate() {
-      database.ref().on("value", function(snapshot) {
-        var sv = snapshot.val();
-        var newRow = $("<tr>").append(
-          `<td>${sv.trainName}</td>
-                <td>${sv.destination}</td>
-                <td>${sv.frequency}</td>
-                <td>${arrivalTime}</td>
-                <td>${tMinutesTillTrain}</td>`
-          );
-      })
-
-    }
-
-
-
-
 
     // code to get the train information
     var firstTimeConverted = moment(firstTtimeDB, "HH:mm A");
@@ -94,25 +69,27 @@ $('document').ready(function() {
 
 
     var sv = snapshot.val();
-console.log(snapshot.key)
+    console.log(snapshot.key)
     var newRow = $(`<tr data-id="${snapshot.key}">`).append(
       `<td>${sv.trainName}</td>
             <td>${sv.destination}</td>
             <td>${sv.frequency}</td>
             <td>${arrivalTime}</td>
             <td>${tMinutesTillTrain}</td>
-            <td><a class="pl-2"> <i class="fas fa-pencil-alt edit-btn"></i></a>
-            <a class="pl-5"> <i class="fas fa-minus-circle delete-btn"></i></a></td>`
+            // I tried to add a working edit and delete button
+            <td><a> <i class="fas fa-pencil-alt edit-btn"></i></a>
+            <a> <i class="fas fa-minus-circle edit-btn"></i></a></td>`
     );
 
     $("#train-tracker > tbody").append(newRow);
   })
 
-$(document).on("click", ".edit-btn", function() {
-var rowId = $(this).parents("tr").attr("data-id")
-rowId.html("<td><input type='text'></td>")
+  // buttons to edit and delete (not working)
+  $(document).on("click", ".edit-btn", function() {
+    var rowId = $(this).parents("tr").attr("data-id")
+    console.log(rowId)
 
-})
+  })
 
-// document.ready closing bracket
+  // document.ready closing bracket
 })
